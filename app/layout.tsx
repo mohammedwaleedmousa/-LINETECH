7ddb83d7601a9c694f525cd2bc08ab9457c3bf00
Mohammed Waleed <aiengineer77@icloud.com>
2026-09-13 18:02:53 +0300

    Load project case study styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,7 @@ import "./final-lock.css";
 import "./hero-variants.css";
 import "./hero-images.css";
 import "./service-detail.css";
+import "./project-detail.css";
 import "./search.css";
 
 const siteDescription = "LINETECH is a technology company that turns ideas into real digital products through strategy, design and engineering.";
