ddd3e40741d57425d5b555c093e285deec12b1f2
Mohammed Waleed <aiengineer77@icloud.com>
2026-09-14 19:54:35 +0300

    Load service engagement styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,6 +42,7 @@ import "./language-startup.css";
 import "./home-neurons.css";
 import "./site-home-theme.css";
 import "./service-detail-compact.css";
+import "./service-detail-engagement.css";
 import "./service-detail-hero-unified.css";
 import "./footer-enhanced.css";
 import "./footer-compact.css";
